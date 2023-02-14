76b8154638aa69e56254f518de69d39a6e6b6c01
CaioSC01 <caio180g@gmail.com>
2023-02-14 16:23:30 -0300

    aprendendo a usar malloc

--- Jogo Pac-man/FogeFoge.c
+++ Jogo Pac-man/FogeFoge.c
@@ -1,12 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char **mapa;
+int linhas;
+int colunas;
+
+void liberaMapa()
+{
+	for (int i = 0; i < linhas; i++)
+	{
+		free(mapa[i]);
+	}
+	free(mapa);
+}
+
+void alocaMapa()
+{
+	mapa = malloc(sizeof(char *) * linhas);
+	for (int i = 0; i < linhas; i++)
+	{
+		mapa[i] = malloc(sizeof(char) * (colunas + 1));
+	}
+}
+
+void leMapa()
+{
+	FILE *f;
+	f = fopen("mapa.txt", "r");
+	if (f == 0)
+	{
+		printf("erro na leitura do mapa\n");
+		exit(1);
+	}
+
+	fscanf(f, "%d %d", &linhas, &colunas);
+
+	alocaMapa();
+
+	for (int i = 0; i < 5; i++)
+	{
+		fscanf(f, "%s", mapa[i]);
+	}
+	fclose(f);
+}
 
 int main()
 {
-	// Matrix de 5 x 10
-	char mapa[5][10];
+	leMapa();
 
-	mapa[0][0] = '|';
-	mapa[4][9] = '@';
-
-	printf("%c %c", mapa[0][0], mapa[4][9]);
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%s\n", mapa[i]);
+	}
+	liberaMapa();
 }
